baf3980af67830874e7acfd7374a8679a5eeb3b0
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-05-26 00:38:37 -0300

    Reducer, store index Typescript

--- PI-Dogs-main/client/src/store/index.ts
+++ PI-Dogs-main/client/src/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from "../reducer";
+import rootReducer from "../reducer/index.ts";
 
 export const store = createStore(
   rootReducer,
