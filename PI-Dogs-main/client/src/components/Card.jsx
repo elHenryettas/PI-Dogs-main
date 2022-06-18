1426da4a6a6b21d66aab6e78cf30c93ca57a1034
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-06-18 16:42:51 -0300

    .

--- PI-Dogs-main/client/src/components/Card.jsx
+++ PI-Dogs-main/client/src/components/Card.jsx
@@ -3,21 +3,30 @@ import { Link } from "react-router-dom";
 
 import style from "./Card.module.css";
 
+const tranformar = (string) => {
+    
+  while(string.length >= 55){
+    let arr = string.split(",")
+    arr.pop()
+    string = arr.join()
+  }
+  return string
+};
+
 export default function Card({ name, image, temperament, id}) {
   return (
     <div className={style.card} key={id}>
       <div className={style.imgConteiner}>
-        <img className={style.img} src={image} alt="A_dog" />
+        <img className={style.img} src={image} alt="A_dog" /> <Link to={`/home/${id}`}>
+          <button className={style.btn}>More Info</button>
+        </Link>
       </div>
       <div>
         <h3>{name}</h3>
-       
        {/*  <h3>{weightMin}</h3> */}
-        <h4>Temperaments</h4>
-        <p>{temperament}</p>
-        <Link to={`/home/${id}`}>
-          <button className={style.btn}>More Info</button>
-        </Link>
+        <h4>Temperaments: </h4>
+        <p>{tranformar(temperament)}</p>
+       
       </div>
     </div>
   );
